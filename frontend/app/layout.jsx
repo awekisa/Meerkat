cba9bdbdc4fd97404172509b1e3295061de5c5db
dimitar.valchev <dimitar.valtchev@gmail.com>
2023-09-22 16:43:06 +0300

    add wallet support for sepolia to app

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,36 +1,38 @@
-"use client";
-import { WagmiConfig, createConfig } from "wagmi";
-import { ConnectKitProvider, getDefaultConfig } from "connectkit";
-import Navbar from "../components/instructionsComponent/navigation/navbar";
-import Footer from "../components/instructionsComponent/navigation/footer";
+'use client';
+import { WagmiConfig, createConfig } from 'wagmi';
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import Navbar from '../components/instructionsComponent/navigation/navbar';
+import Footer from '../components/instructionsComponent/navigation/footer';
+import { mainnet, sepolia } from 'wagmi/chains';
 
 const config = createConfig(
 	getDefaultConfig({
 		// Required API Keys
 		alchemyId: process.env.ALCHEMY_API_KEY, // or infuraId
-		walletConnectProjectId: "demo",
+		walletConnectProjectId: 'demo',
 
 		// Required
-		appName: "You Create Web3 Dapp",
+		appName: 'You Create Web3 Dapp',
 
+		chains: [mainnet, sepolia],
 		// Optional
-		appDescription: "Your App Description",
-		appUrl: "https://family.co", // your app's url
-		appIcon: "https://family.co/logo.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
+		appDescription: 'Your App Description',
+		appUrl: 'https://family.co', // your app's url
+		appIcon: 'https://family.co/logo.png', // your app's logo,no bigger than 1024x1024px (max. 1MB)
 	})
 );
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang='en'>
 			<WagmiConfig config={config}>
-				<ConnectKitProvider mode="dark">
+				<ConnectKitProvider mode='dark'>
 					<body>
 						<div
 							style={{
-								display: "flex",
-								flexDirection: "column",
-								minHeight: "105vh",
+								display: 'flex',
+								flexDirection: 'column',
+								minHeight: '105vh',
 							}}
 						>
 							<Navbar />
